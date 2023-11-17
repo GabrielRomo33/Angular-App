9f197e2bc65a42f4405e6e95ecfaf09df806aba6
GabrielRomo33 <rromo@grand.com.mx>
2023-11-17 12:25:24 -0600

    ng first commit

---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+})
+export class CounterComponent {
+  public counter: number = 10;
+
+
+  increaseBy(value: number){
+    this.counter = this.counter + value;
+  }
+
+  decreaseBy(value: number){
+    this.counter = this.counter - value;
+  }
+}
